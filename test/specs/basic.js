8bbedba5fd7be86e09dbc636fe0279b90dc21520
Robert Zhang <Robert.Zhang@dorsetsoftware.com>
2020-02-04 10:51:28 +0000

    edit 2 0402

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,12 +1,11 @@
-const assert = require('assert')
+const assert = require('assert');
 //import { multiremote } from 'webdriverio'
 
 describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-    	let a = 2;
-        browser.url('https://webdriver.io')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
-        assert.strictEqual(a, 3)
-    })
-})
+	it('should have the right title', () => {
+		let a = 2;
+		browser.url('https://webdriver.io');
+		const title = browser.getTitle();
+		assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+	});
+});
