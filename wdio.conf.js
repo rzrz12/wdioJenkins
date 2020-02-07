b574fd34f931da34382530bea91f56920f748d9e
Robert Zhang <Robert.Zhang@dorsetsoftware.com>
2020-02-07 14:24:59 +0000

    local ip proxy

--- wdio.conf.js
+++ wdio.conf.js
@@ -130,7 +130,7 @@ exports.config = {
 	},
 */
 	seleniumInstallArgs: {
-		proxy: 'http://172.20.224.1:3128/'
+		proxy: 'http://10.1.11.104:3128/'
 		/*
         javaArgs: [
        '-Dwebdriver.ie.driver="C:\Users\robertzhang\Downloads\IEDriverServer.exe"'
