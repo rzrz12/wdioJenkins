cac970ba797846f35135db7b73b3bd0b3993d73d
Robert Zhang <Robert.Zhang@dorsetsoftware.com>
2020-02-24 13:21:53 +0000

    cv site test

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
 //import { multiremote } from 'webdriverio'
 
-describe('webdriver.io page', () => {
-	it('should have the right title', () => {
-		let a = 2;
-		browser.url('https://webdriver.io');
-		const title = browser.getTitle();
-		assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+describe('Joe Bloggs CV page', () => {
+	it('should have the right heading', () => {
+		browser.url('http://dswks125:8088');
+		$('h1').waitForDisplayed();
+		const heading = $('h1').getText();
+		console.log(heading);
+		assert.strictEqual(heading, 'Joe Bloggs');
 	});
 });
