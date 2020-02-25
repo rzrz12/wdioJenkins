db22a0f16d608e461f1a2f494f81d6c60aabb632
Robert Zhang <Robert.Zhang@dorsetsoftware.com>
2020-02-25 11:23:04 +0000

    sauce test, deploy

--- wdio.conf - local.js
+++ wdio.conf - local.js
@@ -8,23 +8,6 @@ exports.config = {
 	// on a remote machine).
 	runner: 'local',
 	//
-	// =================
-	// Service Providers
-	// =================
-	// WebdriverIO supports Sauce Labs, Browserstack, and Testing Bot (other cloud providers
-	// should work too though). These services define specific user and key (or access key)
-	// values you need to put in here in order to connect to these services.
-	//
-	user: process.env.SAUCE_USERNAME,
-	key: process.env.SAUCE_ACCESS_KEY,
-	sauceConnect: true,
-	//
-	// If you run your tests on SauceLabs you can specify the region you want to run your tests
-	// in via the `region` property. Available short handles for regions are `us` (default) and `eu`.
-	// These regions are used for the Sauce Labs VM cloud and the Sauce Labs Real Device Cloud.
-	// If you don't provide the region it will default for the `us`
-	region: 'eu',
-	//
 	// ==================
 	// Specify Test Files
 	// ==================
@@ -61,19 +44,33 @@ exports.config = {
 	// https://docs.saucelabs.com/reference/platforms-configurator
 	//
 	capabilities: [
+		// maxInstances can get overwritten per capability. So if you have an in-house Selenium
+		// grid with only 5 firefox instances available you can make sure that not more than
+		// 5 instances get started at a time.
+		//{   browserName: 'chrome' }
+
+		/**/
 		{
-			// maxInstances can get overwritten per capability. So if you have an in-house Selenium
-			// grid with only 5 firefox instances available you can make sure that not more than
-			// 5 instances get started at a time.
-			maxInstances: 5,
-			//
-			browserName: 'firefox'
-			// If outputDir is provided WebdriverIO can capture driver session logs
-			// it is possible to configure which logTypes to include/exclude.
-			// excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-			// excludeDriverLogs: ['bugreport', 'server'],
+			browserName: 'chrome',
+			'goog:chromeOptions': {
+				useAutomationExtension: false
+			}
 		}
+		/*
+        {
+            browserName: 'Edge'
+        },
+        
+        {
+            browserName: 'internet explorer'
+        }*/
 	],
+
+	//browserName: 'chrome',
+	// If outputDir is provided WebdriverIO can capture driver session logs
+	// it is possible to configure which logTypes to include/exclude.
+	// excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+	// excludeDriverLogs: ['bugreport', 'server'],
 	//
 	// ===================
 	// Test Configurations
@@ -105,7 +102,7 @@ exports.config = {
 	// with `/`, the base url gets prepended, not including the path portion of your baseUrl.
 	// If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
 	// gets prepended directly.
-	baseUrl: 'http://localhost',
+	baseUrl: 'http://testyourlog.in/',
 	//
 	// Default timeout for all waitFor* commands.
 	waitforTimeout: 10000,
@@ -117,12 +114,23 @@ exports.config = {
 	// Services take over a specific job you don't want to take care of. They enhance
 	// your test setup with almost no effort. Unlike plugins, they don't add new
 	// commands. Instead, they hook themselves up into the test process.
-	services: [ 'sauce' ],
-	/*
-	
+	services: [ 'selenium-standalone' ],
+	seleniumLogs: './context/selenium-logs',
+	seleniumArgs: {
+		drivers: {
+			chrome: {
+				version: '79.0.3945.36'
+			}
+		}
+	},
+
 	seleniumInstallArgs: {
-		proxy: 'http://127.0.0.1:3128/'
-	},*/
+		drivers: {
+			chrome: {
+				version: '79.0.3945.36'
+			}
+		}
+	},
 
 	// Framework you want to run your specs with.
 	// The following are supported: Mocha, Jasmine, and Cucumber
